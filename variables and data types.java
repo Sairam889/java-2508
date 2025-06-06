edaf7f87a686b4666abeb526364d370c24b2cec1
sairam <sairamsodasani39@gmail.com>
2025-06-06 22:03:47 +0530

    task1

---
+++ variables and data types.java
@@ -0,0 +1,16 @@
+class HelloWorld {
+    public static void main(String[] args) {
+        String brandName = "HIGHLANDER";
+        Double rating = 4.6;
+        int mrp = 3949;
+        int discount = 81;
+        int price = 750;
+
+        System.out.println("Brand Name : " + brandName);
+        System.out.println("Ratings : " + rating + "*" );
+        System.out.println("MRP : " + mrp + "rs");
+        System.out.println("Discount : " + discount + "% OFF");
+        System.out.println("Prcie : " + price + "Rs");
+
+    }
+}
