9108c14bde5c308cf2732e73d44719438332e727
sairam <sairamsodasani39@gmail.com>
2025-06-13 08:18:25 +0530

    application form-task(2)

---
+++ admission_application.java
@@ -0,0 +1,48 @@
+public class admission_application {
+    public static void main(String[] args) {
+        
+        System.out.println("APPLICATION FOR COLLEGE ADMISSION TEST");
+
+        String firstName = "Yuji";
+        String lastName = "itadori";
+        byte age = 18;
+        char gender = 'M';
+        short dob = 01;
+        int contactNum = 123456789;
+        
+
+        String fatherName = "Yuji Satoru";
+        String occupation = "Professor";
+        int contactNumb = 456789123;
+        String motherName = "Yuji Umeko";
+        String occupationn = "Teacher";
+        int contactNumber = 987456123;
+
+        String previousSchool = "Jujutsu Tech";
+        float cgpa = 8.98f;
+        short yearOfPassing = 2025;
+        boolean disability = false;
+
+        System.out.println("First Name : " + firstName);
+        System.out.println("Last Name : " + lastName);
+        System.out.println("Age : " + age);
+        System.out.println("Gender : " + gender);
+        System.out.println("Date of Birth : " + dob + "/1/2007");
+        System.out.println("Contact Number : " + contactNum);
+        System.out.println("Father Name : " + fatherName);
+        System.out.println("Occupation : " + occupation);
+        System.out.println("Contact Number : " + contactNumb);
+        System.out.println("Mother Name : " + motherName);
+        System.out.println("Occupation : " + occupationn);
+        System.out.println("Contact Number : " + contactNumber);
+
+        System.out.println("Previous School : " + previousSchool);
+        System.out.println("Year of passing : " + yearOfPassing);
+        System.out.println("Disability : " + disability);
+        
+
+
+
+    }
+
+}
