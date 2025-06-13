041cb21ec4871bd5ad56c12d27c9f4645176d786
Sairam889 <124248123+Sairam889@users.noreply.github.com>
2025-06-13 08:26:41 +0530

    Class

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,35 +1,7 @@
 class HelloWorld {
     public static void main(String[] args) {
-        String brandName = "HIGHLANDER";
-        Double rating = 4.6;
-        int mrp = 3949;
-        int discount = 81;
-        int price = 750;
-
-        String name = "Mahindra Thar";
-        int engine = 1497; //cc
-        double power = 116.93;
-        int seatingCapacity = 4;
-        int ratings = 4;
-        int torque = 300;
-        int driveType = 4;
-
         
-
-        System.out.println("Brand Name : " + brandName);
-        System.out.println("Ratings : " + rating + "*" );
-        System.out.println("MRP : " + mrp + "rs");
-        System.out.println("Discount : " + discount + "% OFF");
-        System.out.println("Prcie : " + price + "Rs");
-        System.out.println("============");
-
-        System.out.println("Car" + name);
-        System.out.println("Engine" + engine + " - 2184cc");
-        System.out.println("Power" +  power + " - 150.19 bhp");
-        System.out.println("Seating Capacity" + seatingCapacity);
-        System.out.println("Rating" + ratings + " Star");
-        System.out.println("Torque" +  torque + " - 320 Nm");
-        System.out.println("Drive Type" + driveType + "WD / RWD");
+        System.out.println("Hello");
 
 
 
